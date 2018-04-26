1b1b3cfb5d41263e70c764f690edd61794082b18
Siliang <lusiliang93@gmail.com>
2018-04-26 15:43:01 -0400

    new predict

--- predict.py
+++ predict.py
@@ -1,19 +1,28 @@
 import requests
 import time
+import numpy as np
+import pickle
+from sklearn import datasets, svm
 
-sns.set(color_codes=True)
 
 filename = 'svm_model.sav'
 load_model = pickle.load(open(filename,'rb'))
 
 while (True):
 	# get parameters from particle cloud
-	r = requests.get('https://api.particle.io/v1/devices/1f0028000c47343438323536/test?access_token=062530ccee58bc2babf77e8d1bc8500e4ac44014')
-	print r.json()['result']
+	humidity = requests.get('https://api.particle.io/v1/devices/28003a000c47343438323536/humidity?access_token=8608ee061d524b0682c1880998688651a14b73de')
+	#print(r.json()['result'])
+	h = humidity.json()['result']
+	temperature = requests.get('https://api.particle.io/v1/devices/28003a000c47343438323536/temperature?access_token=8608ee061d524b0682c1880998688651a14b73de')
+	t = temperature.json()['result']
+	skin = requests.get('https://api.particle.io/v1/devices/28003a000c47343438323536/skin?access_token=8608ee061d524b0682c1880998688651a14b73de')
+	s = skin.json()['result']
+	print(h,t,s)
 	# example
 	# features: temperature_normal, humidity_normal, skin_normal, clothing
 	test = np.array([[-1.785714,-0.457604,-2.04924,21.7]])
 	pred = load_model.predict(test)
+	
 
 
 	# sleep for 3 seconds
